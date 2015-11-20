c6cc28afd66c5342cc8debd875060d70b17886ef
iitc <iancaffey@ymail.com>
2015-11-20 14:57:28 -0600

    Added interior collision checking to avoid objects being perpetually stuck vibrating against each other

--- src/iitc/game/Scene.java
+++ src/iitc/game/Scene.java
@@ -1,7 +1,6 @@
 package iitc.game;
 
-import iitc.physics.Dimension2d;
-import iitc.physics.Motion;
+import iitc.physics.*;
 import iitc.physics.Rectangle;
 import iitc.physics.Vector;
 
@@ -61,11 +60,19 @@ public class Scene {
         updateTimes.clear();
     }
 
+    //TODO:Do some pruning to the entity sets for collisions to optimize larger entity sets
     public synchronized void update() {
         synchronized (entities) {
             Map<SceneEntity2d, List<SceneEntity2d>> visited = new HashMap<SceneEntity2d, List<SceneEntity2d>>();
             for (SceneEntity2d entity : entities) {
+                Cartesian2d entityPosition = entity.getPosition();
+                Vector2d entityVelocity = entity.getVelocity();
+                Dimension2d entitySize = entity.getSize();
                 List<SceneEntity2d> entityVisits = visited.get(entity);
+                double entityX = entityPosition.getX();
+                double entityY = entityPosition.getY();
+                double entityMaxX = entityX + entitySize.getWidth();
+                double entityMaxY = entityY + entitySize.getHeight();
                 Rectangle rectangle = new Rectangle(entity.getPosition(), entity.getSize());
                 for (SceneEntity2d opposition : entities) {
                     if ((entityVisits != null && entityVisits.contains(opposition)) || opposition.equals(entity))
@@ -73,10 +80,19 @@ public class Scene {
                     List<SceneEntity2d> oppositeVisits = visited.get(opposition);
                     if (oppositeVisits != null && oppositeVisits.contains(entity))
                         continue;
-                    if (rectangle.intersects(opposition.getPosition(), opposition.getSize())) {
+                    Cartesian2d oppositionPosition = opposition.getPosition();
+                    Vector2d oppositionVelocity = opposition.getVelocity();
+                    Dimension2d oppositionSize = opposition.getSize();
+                    if (rectangle.intersects(oppositionPosition, oppositionSize)) {
+                        double oppositionX = oppositionPosition.getX();
+                        double oppositionY = oppositionPosition.getY();
+                        double oppositionMaxX = oppositionX + oppositionSize.getWidth();
+                        double oppositionMaxY = oppositionY + oppositionSize.getHeight();
+                        if ((entityX < oppositionX && oppositionMaxX > entityMaxX && entityVelocity.getX() < 0 && oppositionVelocity.getX() > 0) || (entityY < oppositionY && oppositionMaxY > entityMaxY && entityVelocity.getY() < 0 && oppositionVelocity.getY() > 0))
+                            continue;
                         //TODO:Actually get the proper collision calculating momentum and using proper angles
-                        entity.setVelocity(Vector.inverse(entity.getVelocity()));
-                        opposition.setVelocity(Vector.inverse(opposition.getVelocity()));
+                        entity.setVelocity(Vector.inverse(entityVelocity));
+                        opposition.setVelocity(Vector.inverse(oppositionVelocity));
                         if (entityVisits == null) {
                             entityVisits = new ArrayList<SceneEntity2d>();
                             visited.put(entity, entityVisits);
@@ -87,10 +103,19 @@ public class Scene {
                 for (SceneEntity2d opposition : walls) {
                     if ((entityVisits != null && entityVisits.contains(opposition)) || opposition.equals(entity))
                         continue;
-                    if (rectangle.intersects(opposition.getPosition(), opposition.getSize())) {
+                    Cartesian2d oppositionPosition = opposition.getPosition();
+                    Vector2d oppositionVelocity = opposition.getVelocity();
+                    Dimension2d oppositionSize = opposition.getSize();
+                    if (rectangle.intersects(oppositionPosition, oppositionSize)) {
+                        double oppositionX = oppositionPosition.getX();
+                        double oppositionY = oppositionPosition.getY();
+                        double oppositionMaxX = oppositionX + oppositionSize.getWidth();
+                        double oppositionMaxY = oppositionY + oppositionSize.getHeight();
+                        if ((entityX < oppositionX && oppositionMaxX > entityMaxX && entityVelocity.getX() < 0 && oppositionVelocity.getX() > 0) || (entityY < oppositionY && oppositionMaxY > entityMaxY && entityVelocity.getY() < 0 && oppositionVelocity.getY() > 0))
+                            continue;
                         //TODO:Actually get the proper collision calculating momentum and using proper angles
-                        entity.setVelocity(Vector.inverse(entity.getVelocity()));
-                        opposition.setVelocity(Vector.inverse(opposition.getVelocity()));
+                        entity.setVelocity(Vector.inverse(entityVelocity));
+                        opposition.setVelocity(Vector.inverse(oppositionVelocity));
                         if (entityVisits == null) {
                             entityVisits = new ArrayList<SceneEntity2d>();
                             visited.put(entity, entityVisits);
